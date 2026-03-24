6dc8465b2bf750d586698a3760f9c764969cae66
Net00s <Neo07@jutstrom.se>
2026-03-24 16:24:40 +0100

    Working

--- WebsiteLoader.js
+++ WebsiteLoader.js
@@ -1,1 +1,15 @@
-var usableList = nameList.split(")")
+var usableList = nameList.split(")");
+
+let nameP = document.getElementById("Name");
+let franchiseP = document.getElementById("Franchise")
+
+
+
+function Load(){
+    chosenCharacter = usableList[Math.floor(Math.random()*usableList.length)].split(" (");
+
+    console.log(chosenCharacter)
+
+    nameP.innerHTML = chosenCharacter[0];
+    franchiseP.innerHTML = chosenCharacter[1];
+}
