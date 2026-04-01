47da57964569af9e79dceb1ca559ca6eacebaab7
simolly0 <simon.ebestam@elev.ga.lbs.se>
2026-04-02 01:24:36 +0200

    updating my personal notes

--- ListLoader.js
+++ ListLoader.js
@@ -4655,4 +4655,4 @@ Chug (Planes)
 ()
 `
 
-// Franchises added: Skylanders, Blazblue, Granblue, DNF Duel, Guilty Gear, Mortal Kombat, Street Fighter, Under Night In-Birth, Pokemon Species, Dead By Daylight Original Killers, Star Wars, MArvel, Lorigins, Risk Of Rain, 
+// Franchises added: Skylanders, Blazblue, Granblue, DNF Duel, Guilty Gear, Mortal Kombat, Street Fighter, Under Night In-Birth, Pokemon Species, Dead By Daylight Original Killers, Star Wars, MArvel, Lorigins, Risk Of Rain, Deadlock, Team Fortress 2, Overwatch, Hermitcraft, Arknights, How To Train Your Dragon, Five Nights At Freddys, Famous People, Carrion, DOOM, SANDA, My Hero Academia, Cars, Planes, 
